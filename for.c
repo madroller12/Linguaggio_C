e40d37d5b83b2bbb3aa6b4cc746f0ecfdc10dd91
Vladyslav <vladyslavblonskiy12@gmail.com>
2025-10-28 08:46:47 +0000

    first commit

---
+++ for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int pippo[10];
+    int i;
+
+    for (i = 0; i < 10; i++){
+        printf("Inserisci un numero intero \n");
+        scanf("%d", &pippo[i]);
+    }
+
+    printf("L'ultimo numero è %d \n", pippo[i-1]);
+
+    return 0;
+}
