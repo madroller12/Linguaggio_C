90c0e5559947cbe90a385c7307c39e46a04e07df
Vladyslav <vladyslavblonskiy12@gmail.com>
2025-11-04 08:01:54 +0000

    Add comments to file

--- 04.11/04.11.c
+++ 04.11/04.11.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    // Dichiara un array di 10 numeri interi
     int array[10];
     int i, j, temp;
 
+    // Chiede all'utente di inserire 10 numeri
     printf("Inserisci %d numeri interi:\n", 10);
+    // Ciclo per l'inserimento dei numeri
     for (i = 0; i < 10; i++) {
         printf("Elemento %d: ", i + 1);
         scanf("%d", &array[i]);
     }
 
+    // Implementazione del Bubble Sort per ordinare l'array
     for (i = 0; i < 10 - 1; i++) {
+        // Confronta coppie di elementi adiacenti
         for (j = 0; j < 10 - i - 1; j++) {
+            // Se l'elemento corrente è maggiore del successivo, li scambia
             if (array[j] > array[j+1]) {
                 temp = array[j];
                 array[j] = array[j+1];
@@ -20,6 +26,7 @@ int main() {
         }
     }
 
+    // Stampa l'array ordinato
     printf("\nArray ordinato in ordine crescente:\n");
     for (i = 0; i < 10; i++) {
         printf("%d ", array[i]);
