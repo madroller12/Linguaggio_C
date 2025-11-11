fb4e4107795186835bbe61fe46f109115ec913b7
Vladyslav <vladyslavblonskiy12@gmail.com>
2025-11-11 09:31:18 +0000

    exercise 11.11 (2.0)

--- 11.11/11.11.c
+++ 11.11/11.11.c
@@ -13,14 +13,23 @@ int main() {
             break;
         }
     }
-    printf("Fine del programma. Numero: %d\n", n_s);
+    printf("Numero succesivo di %d: %d\n", numero, n_s);
     n_s = n_s / 2;
     risultato = floor(log2(n_s));
     printf("Il logaritmo in base 2 di %d e' %.0f\n", n_s, risultato);
-    int risultato2 = (int)risultato;
-    risultato2 = risultato2 + 1;
+    int risultato2 = (int)risultato + 2;
     char arr[risultato2];
-
+    for (int i = 0; i < risultato2-1; i++) {
+        if (numero >= n_s) {
+            arr[i] = '1';
+            numero = numero - n_s;
+        } else {
+            arr[i] = '0';
+        }
+        n_s = n_s / 2;
+        arr[i + 1] = '\0';
+    }
+    printf("La rappresentazione binaria e': %s\n", arr);
 
     return 0;
 }
